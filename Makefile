ebf54230e7cd936f8e3aea298f957cbe285592dc
Darrow O'Lykos <caffeine-yeeter@protonmail.com>
2022-02-25 15:14:48 -0500

    update

---
+++ Makefile
@@ -0,0 +1,5 @@
+push:
+	git add --all && git commit -m "update" && git push;
+
+generate_docs:
+	cargo doc
--- doc/search-index.js
+++
@@ -1,4 +0,0 @@
-var searchIndex = JSON.parse('{\
-"lib":{"doc":"","t":[5],"n":["hello"],"q":["lib"],"d":[""],"i":[0],"f":[[[]]],"p":[]}\
-}');
-if (window.initSearch) {window.initSearch(searchIndex)};
